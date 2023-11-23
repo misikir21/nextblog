3ad703a0e113477a6a834b54b141f28538a67caf
misikir21 <misikirkaka@gmail.com>
2023-11-23 19:52:27 +0300

    fix all errors

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,18 +1,17 @@
 import Image from "next/image";
 import Logo from "./dojo-logo.png";
 import Link from "next/link";
-export default function navbar() {
+export default function Navbar() {
   return (
     <nav>
       <Image
         src={Logo}
         alt="dojo logo"
-        width={50}
-        height={50}
+        width={70}
         quality={100}
         placeholder="blur"
       />
-      <h1>Dojo h1elpdesk</h1>
+      <h1>Dojo helpdesk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
     </nav>
