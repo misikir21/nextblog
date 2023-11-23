971214785cc867ffa7dcb5a0f8f4c6933d7d56dd
misikir21 <misikirkaka@gmail.com>
2023-11-23 19:30:46 +0300

    add more element to home dashboard

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,18 +1,20 @@
-import React from "react";
 import Image from "next/image";
-import logo from "./dojo-logo.png";
-
+import Logo from "./dojo-logo.png";
+import Link from "next/link";
 export default function navbar() {
   return (
     <nav>
       <Image
-        src={logo}
+        src={Logo}
         alt="dojo logo"
         width={50}
         height={50}
         quality={100}
         placeholder="blur"
       />
+      <h1>Dojo h1elpdesk</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
     </nav>
   );
 }
