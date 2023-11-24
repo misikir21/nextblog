efbecc7a0902f92210df9f64eb46737be9d0046b
misikir21 <misikirkaka@gmail.com>
2023-11-24 19:49:01 +0300

    fix errors

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,21 +1,23 @@
-import React from "react";
 async function getTicket(id) {
-  const res = await fetch("http:localhost:4000/tickets", +id, {
+  const res = await fetch(`http://localhost:4000/tickets/${id}`, {
     next: {
       revalidate: 60,
     },
   });
+
   return res.json();
 }
-export default async function TicketsDetail({ params }) {
+
+export default async function TicketDetails({ params }) {
+  // const id = params.id
   const ticket = await getTicket(params.id);
-  const id = params.id;
+
   return (
     <main>
       <nav>
         <h2>Ticket Details</h2>
       </nav>
-      <div className="car">
+      <div className="card">
         <h3>{ticket.title}</h3>
         <small>Created by {ticket.user_email}</small>
         <p>{ticket.body}</p>
