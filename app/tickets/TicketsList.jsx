c90746c3e2147e1cf3606d1afffb9a394c6bc0de
misikir21 <misikirkaka@gmail.com>
2023-11-24 09:20:41 +0300

    add a feature to display tickets form local server

--- app/tickets/TicketsList.jsx
+++ app/tickets/TicketsList.jsx
@@ -1,5 +1,23 @@
-import React from "react";
-
-export default function TicketsList() {
-  return <div>TicketsList</div>;
+async function getTickets() {
+  const res = await fetch("http:localhost:4000/tickets");
+  return res.json();
+}
+export default async function TicketsList() {
+  const tickets = await getTickets();
+  return (
+    <>
+      {tickets.map((ticket) => (
+        <div key={ticket.id} className="card my-5">
+          <h3>{ticket.title}</h3>
+          <p>{ticket.body.slice(0, 200)}...</p>
+          <div className={`pill ${ticket.priorty}`}>
+            {ticket.priorty} priority
+          </div>
+        </div>
+      ))}
+      {tickets.length === 0 && (
+        <p className="text-center">There is no open ticket</p>
+      )}
+    </>
+  );
 }
