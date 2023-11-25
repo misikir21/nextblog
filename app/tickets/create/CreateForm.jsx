8831d151958111a10ac205e495158a4ef26be00d
misikir21 <misikirkaka@gmail.com>
2023-11-25 11:18:08 +0300

    add a featuer to submit ticket

--- app/tickets/create/CreateForm.jsx
+++ app/tickets/create/CreateForm.jsx
@@ -1,25 +1,41 @@
 "use client";
-import { useRouter } from "next/router";
+
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+
 export default function CreateForm() {
-  const router = useRouter;
+  const router = useRouter();
+
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [priority, setPriority] = useState("low");
   const [isLoading, setIsLoading] = useState(false);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsLoading(true);
-    const ticket = {
+
+    const newTicket = {
       title,
       body,
       priority,
-      user_email: "miskirteka@gmail.com",
+      user_email: "mario@netninja.dev",
     };
+
+    const res = await fetch("http://localhost:4000/tickets", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newTicket),
+    });
+
+    if (res.status === 201) {
+      router.refresh();
+      router.push("/tickets");
+    }
   };
 
   return (
-    <form className="w-1/2">
+    <form onSubmit={handleSubmit} className="w-1/2">
       <label>
         <span>Title:</span>
         <input
@@ -30,7 +46,7 @@ export default function CreateForm() {
         />
       </label>
       <label>
-        <span>Body:</span>
+        <span>Title:</span>
         <textarea
           required
           onChange={(e) => setBody(e.target.value)}
