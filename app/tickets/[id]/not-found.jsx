1be23c3add4a82db1eaa67affc94e2aaaa7c05f7
misikir21 <misikirkaka@gmail.com>
2023-11-25 09:24:47 +0300

    add a custom 404 page for tickets only

---
+++ app/tickets/[id]/not-found.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+export default function Notfound() {
+  return (
+    <main className="text-center">
+      <h2 className="text-3xl">We Hit a Brick Wall</h2>
+      <p>we could not find the ticket you were looking for</p>
+      <p>
+        Go back to dashboard<Link href="/tickets">Tickets</Link>
+      </p>
+    </main>
+  );
+}
